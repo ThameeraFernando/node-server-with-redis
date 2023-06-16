6ac8f6171d56159dc2a4cf7311ef98e223bc89f0
ThameeraFernando <sithnuwan111@gmail.com>
2023-06-16 21:26:45 +0530

    cache the fetch requests using redis

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const axios = require("axios");
+const redis = require("redis");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+let redisClient;
+(async () => {
+  redisClient = redis.createClient();
+  redisClient.on("error", (error) => {
+    console.error(`Error:${error}`);
+  });
+  await redisClient.connect();
+})();
+
+// fetch data from api
 const fetchApiData = async (species) => {
   const apiResponse = await axios.get(
     `https://www.fishwatch.gov/api/species/${species}`
@@ -12,16 +23,25 @@ const fetchApiData = async (species) => {
   return apiResponse.data;
 };
 
+// get species controller
 const getSpeciesData = async (req, res) => {
   const species = req.params.species;
   let result;
+  let isCached = false;
   try {
-    result = await fetchApiData(species);
-    if (result.length === 0) {
-      throw "API returned an empty array";
+    const cacheData = await redisClient.get(species);
+    if (cacheData) {
+      isCached = true;
+      result = JSON.parse(cacheData);
+    } else {
+      result = await fetchApiData(species);
+      if (result.length === 0) {
+        throw "API returned an empty array";
+      }
+      await redisClient.set(species, JSON.stringify(result));
     }
     res.send({
-      fromCache: false,
+      fromCache: isCached,
       data: result,
     });
   } catch (error) {
@@ -30,6 +50,7 @@ const getSpeciesData = async (req, res) => {
   }
 };
 
+// get route
 app.get("/fish/:species", getSpeciesData);
 
 app.listen(PORT, () => {
