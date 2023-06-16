1c11ba4800505bd41b99c9f81b23bac51683e0bf
ThameeraFernando <sithnuwan111@gmail.com>
2023-06-16 20:43:50 +0530

    create a get request and fetch the data from an api

--- server.js
+++ server.js
@@ -1,7 +1,36 @@
 const express = require("express");
+const axios = require("axios");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const fetchApiData = async (species) => {
+  const apiResponse = await axios.get(
+    `https://www.fishwatch.gov/api/species/${species}`
+  );
+  console.log("Request send to the API");
+  return apiResponse.data;
+};
+
+const getSpeciesData = async (req, res) => {
+  const species = req.params.species;
+  let result;
+  try {
+    result = await fetchApiData(species);
+    if (result.length === 0) {
+      throw "API returned an empty array";
+    }
+    res.send({
+      fromCache: false,
+      data: result,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(404).send("Data unavailable");
+  }
+};
+
+app.get("/fish/:species", getSpeciesData);
 
 app.listen(PORT, () => {
   console.log(`Server running on the port ${PORT}`);
